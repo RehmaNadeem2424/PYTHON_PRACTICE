d3caaa757e00842386ccfa338d70b6078bfa55ef
RehmaNadeem <rehmanadeem2424@gmail.com>
2024-09-02 23:53:11 -0700

    new file

---
+++ python_practice/practice1.py
@@ -0,0 +1,155 @@
+quote1 = '"colin powell said" :perpetual optimism is a force multipiler...'
+print(quote1)
+quote2 = 'quaid e azam "said":i shall never come to the punjab again;it ia such a hopeless place'
+print(quote2)
+quote3  = "rehma said : 'spread love everywhere you go'"
+print(quote3)
+quote4 = 'joe jackson:"if you build it, they will come."'
+print(quote4)
+quote5 = ''''nothing' is "impossible"'''
+print(quote5)
+quote6 =''' martin luther king jr...
+"injustice anywhere is a threat to justice everywhere"'''
+print(quote6)
+quote7 = '''"Mother Teresa":
+not all of us can do great things. but we can do small things with great love.'''
+print(quote7)
+quote8 = 'Helen Keller :"The only thing worse than being blind is having sight but no vision."'
+print(quote8)
+quote9 ='Marie Cuire :"Nothing in is life to be feared; it is only to be understood.Now is the time to understand more , so that we may fear less"'
+print(quote9)
+quote10 = "Bee the change that u wish to see in the world" 
+print(quote10)
+quote11 = ''''Nelson Mandela' :
+"Imagination is more important than knowledge.For knowledge is limited ,whereas imagination embraces the entire world."'''
+print(quote11)
+quote12 ='Leonardo da vinci ; "simplicity is the ultimate sophistication"'
+print(quote12)
+quote13 = "you will face many defeats in 'life', but never let 'yourself' be defeated" 
+print(quote13)
+quote14 = '''"Mark Twain":
+"The secret of getting ahead is getting started"'''
+print(quote14)
+quote15 ='''"Rehma Nadeem said" ;
+"it dose not" matter" how slowly you go as long as u do not stop"'''
+print(quote15)
+quote16 = "Abraham Lincoln:''In the end it's not the years in your life that count.It's yhe life in your years.'"
+print(quote16)
+quote17 = '''"Winston Churchill:"
+"success is not final , failure is not fatel:it is the courage to continue that counts.""'''
+print(quote17)
+quote18 = '''"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment."
+Ralph Waldo Emerson'''
+print(quote18)
+quote19 = 'Franklin D. Roosevelt : "The only thing we have to fear is fear itself."'
+print(quote19)
+quote20 = "change the world by being 'yourself.'"
+print(quote20)
+name ='''  
+My
+  name
+       is
+         rehma
+               nadeem
+               i 
+            am
+           a 
+      student
+    of 
+PIAIC'''
+print(name)
+
+message =  '''
+Attitude 
+        is 
+          a
+           little
+                 thing
+             that 
+         makes 
+        a
+       big 
+difference'''
+print(message)
+
+fruits = '''
+Apple
+      Banana
+             Grapes
+                   Cheery
+                          Blue berry
+                     Orange
+                  Pear
+              mango 
+        Avocado                      
+Pineapple'''
+print(fruits)
+
+vegetables = '''
+Capsicum
+        Potato
+              Tomato
+                    Onion
+                         Brinjal
+                   Garlic
+           Beetroot
+       Cabbage
+     Peas                                   
+Carrot'''
+print(vegetables)
+
+places = '''
+        Switzerland
+                   london
+                         paris 
+                               New york
+                                      New Zealand 
+                                Greece 
+                          Turkey
+                  Australia
+             Norway
+     Thailand'''
+print(places)
+
+msg = ''' 
+It 
+  is 
+    never
+         too 
+            late
+                to
+                  be
+             what 
+          s
+      might
+   have
+been'''
+print(msg)
+
+animals = '''
+Elephant
+        Tiger
+             Horse
+                  Monkey
+                         Kangaroo
+                    Lion
+               Duck
+         Rabbit
+     Goat
+Gorilla'''
+print(animals)
+
+birds = '''
+Parrot
+      Sparrow
+             peacock
+                    Eagle
+                          Vulture
+                 Kingfisher
+            Robin
+         Dove
+     Quail
+ Swan'''
+print(birds)
+
+
